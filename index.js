db4f6a3b042e3f1c6f031938b9986ae24d5a7725
davidalds <david57araujo@gmail.com>
2023-07-31 05:55:13 -0300

    add helmet lib

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
 const port = 8000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(helmet());
 
 const whitelist = process.env.WHITELIST.split(',');
 
